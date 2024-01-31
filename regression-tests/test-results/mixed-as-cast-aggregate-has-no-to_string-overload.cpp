11b7acb0f6d04b21e7b18394de9513067effe13c
Filip Sajdak <filip.sajdak@gmail.com>
2024-01-31 23:13:27 +0100

    Add test for as cast aggregate has no to_string overload

---
+++ regression-tests/test-results/mixed-as-cast-aggregate-has-no-to_string-overload.cpp
@@ -0,0 +1,35 @@
+
+
+//=== Cpp2 type declarations ====================================================
+
+
+#include "cpp2util.h"
+
+#line 1 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+
+#line 5 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+class A;
+
+
+//=== Cpp2 type definitions and function declarations ===========================
+
+#line 1 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+auto main() -> int;
+
+#line 5 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+class A {
+      public: A() = default;
+      public: A(A const&) = delete; /* No 'that' constructor, suppress copy */
+      public: auto operator=(A const&) -> void = delete;
+};
+#line 6 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+
+
+//=== Cpp2 function definitions =================================================
+
+#line 1 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+auto main() -> int{
+#line 2 "mixed-as-cast-aggregate-has-no-to_string-overload.cpp2"
+    cpp2::as_<std::string>(std::variant<int,A>(42));
+}
+
