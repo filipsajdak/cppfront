08cd36996328529ffc4577e34d495e95a2c9fa25
Filip Sajdak <filip.sajdak@gmail.com>
2024-01-31 23:13:16 +0100

    Add handling of checking the variant type itself
    In the current code the following code does not work:
    ```cpp
    v : std::variant<std::optional<int>, std::variant<int>> = std::variant(42);
    v is std::variant<std::optional<int>, std::variant<int>>; // returns false
    ```
    After this change it will return true;

--- include/cpp2util.h
+++ include/cpp2util.h
@@ -1775,7 +1775,7 @@ auto is( std::variant<Ts...> const& x ) {
     return type_find_if<Ts...>([&]<typename It>(It const&) -> bool {
         if (x.index() == It::index) { return is<T>(std::get<It::index>(x));}
         return false;
-    }) != std::variant_npos;
+    }) != std::variant_npos || std::same_as<std::variant<Ts...>, T>;
 }
 
 template<template <typename...> class C, specialization_of_template<std::variant> T>
